b11ce22e621d84b5235a34eee3ff0d0dbdf68aca
loubarb <louis.barbati@gmail.com>
2022-05-19 23:54:51 -0400

    Add functions for match checking

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 const playCard = document.querySelectorAll('.play-card');
-
+let flippedCard = false;
+let firstCard, secondCard;
 
 function handleCardFlip() {
-  this.classList.toggle('show');
+  this.classList.add('flip');
+  
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  flippedCard = false;
+
+  checkMatch();
+}
+
+function checkMatch() {
+  if (firstCard.dataset.coin === secondCard.dataset.coin) {
+    disableCards();
+    return;
+  }
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', handleCardFlip);
+  secondCard.removeEventListener('click', handleCardFlip);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1200)
 }
 
 playCard.forEach((card) => {
   card.addEventListener('click', handleCardFlip)
 })
-
-// playCard.addEventListener('click', handleCardFlip)
