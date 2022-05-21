f2ba9c520258dd90f1e864c6e9d940a6afdaf877
loubarb <louis.barbati@gmail.com>
2022-05-20 22:40:11 -0400

    Add function to shuffle board per play

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const playCard = document.querySelectorAll('.play-card');
 let flippedCard = false;
 let firstCard, secondCard;
+let boardLock = false;
 
 function handleCardFlip() {
+  if (boardLock) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
   
   if (!flippedCard) {
@@ -28,14 +31,34 @@ function checkMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', handleCardFlip);
   secondCard.removeEventListener('click', handleCardFlip);
+  resetBoard();
 }
 
 function unflipCards() {
+  boardLock = true;
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+    boardLock = false;
+    resetBoard();
   }, 1200)
 }
+
+function resetBoard() {
+  let flippedCard = false;
+  let boardLock = false;
+  let firstCard = null;
+  let secondCard = null;
+}
+
+function shuffle() {
+  playCard.forEach((card) => {
+    let randomSpot = Math.floor(Math.random() * 18);
+    card.style.order = randomSpot;
+  })
+};
+
+shuffle();
 
 playCard.forEach((card) => {
   card.addEventListener('click', handleCardFlip)
